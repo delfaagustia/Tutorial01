0fea00b3d2ced2f6f5388fd151e450f6bb6742c6
delfaagustia <delfaagustia09@gmail.com>
2018-09-07 14:42:10 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,32 @@
+
+public class Mahasiswa {
+	public String nama;
+	public String nmp;
+	public String jurusan;
+	
+	public static void main (String [] args){
+		system.out.println ("hehe");
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNmp() {
+		return nmp;
+	}
+	public void setNmp(String nmp) {
+		this.nmp = nmp;
+	}
+	public String getJurusan() {
+		return jurusan;
+	}
+	public void setJurusan(String jurusan) {
+		this.jurusan = jurusan;
+	}
+	
+
+}
+}
