e741988e8987e5335a760e177818d88bdefab1c2
delfaagustia <delfaagustia09@gmail.com>
2018-09-07 14:43:30 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,23 @@
+public class Dosen {
+	public String nama;
+	public String nip;
+
+	
+	public static void main (String [] args){
+		system.out.println ("hehe");
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNip() {
+		return nip;
+	}
+	public void setNip(String nip) {
+		this.nip = nip;
+	}
+}
+
